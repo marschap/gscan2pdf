bd420ba6e9923e67f5f8cd6b28792d9b7a3d6b48
Jeffrey Ratcliffe <Jeffrey.Ratcliffe@gmail.com>
2012-09-27 10:12:08 +0200

    Moved code getting options via CLI to Gscan2pdf::Frontend::CLI::find_scan_options

--- lib/Gscan2pdf/Frontend/CLI.pm
+++ lib/Gscan2pdf/Frontend/CLI.pm
@@ -3,6 +3,7 @@ package Gscan2pdf::Frontend::CLI;
 use strict;
 use warnings;
 
+use Carp;
 use Text::ParseWords;
 use Glib qw(TRUE FALSE);
 use POSIX qw(locale_h :signal_h :errno_h :sys_wait_h);
@@ -23,12 +24,75 @@ sub get_devices {
  my ( $class, %options ) = @_;
  my $running = TRUE;
 
+ _run_cmd(
+"$prefix scanimage --formatted-device-list=\"'%i','%d','%v','%m','%t'%n\" 2>/dev/null",
+  $options{running_callback},
+  sub {
+   my ($output) = @_;
+   $options{finished_callback}
+     ->( Gscan2pdf::Frontend::CLI->parse_device_list($output) )
+     if ( defined $options{finished_callback} );
+  }
+ );
+ return;
+}
+
+sub parse_device_list {
+ my ( $class, $output ) = @_;
+
+ my (@device_list);
+
+ $logger->info($output) if ( defined($output) );
+
+ # parse out the device and model names
+ my @words =
+   &parse_line( ',', 0, substr( $output, 0, index( $output, "'\n" ) + 1 ) );
+ while ( @words == 5 ) {
+  $output = substr( $output, index( $output, "'\n" ) + 2, length($output) );
+  push @device_list,
+    {
+   name   => $words[1],
+   vendor => $words[2],
+   model  => $words[3],
+   type   => $words[4]
+    };
+  @words =
+    &parse_line( ',', 0, substr( $output, 0, index( $output, "'\n" ) + 1 ) );
+ }
+
+ return \@device_list;
+}
+
+sub find_scan_options {
+ my ( $class, %options ) = @_;
+
+ # Get output from scanimage or scanadf.
+ # Inverted commas needed for strange characters in device name
+ my $cmd =
+"$options{'scan prefix'} $options{frontend} --help --device-name='$options{device}'";
+ $cmd .= " --mode='$options{mode}'" if ( defined $options{mode} );
+ _run_cmd(
+  $cmd,
+  $options{running_callback},
+  sub {
+   my ($output) = @_;
+   $options{finished_callback}->($output)
+     if ( defined $options{finished_callback} );
+  }
+ );
+ return;
+}
+
+sub _run_cmd {
+ my ( $cmd, $running_callback, $finished_callback ) = @_;
+ my $running = TRUE;
+
  # Timer will run until callback returns false
  my $timer = Glib::Timeout->add(
   $_POLL_INTERVAL,
   sub {
    if ($running) {
-    $options{running_callback}->() if ( defined $options{running_callback} );
+    $running_callback->() if ( defined $running_callback );
     return Glib::SOURCE_CONTINUE;
    }
    else {
@@ -37,8 +101,6 @@ sub get_devices {
   }
  );
 
- my $cmd =
-"$prefix scanimage --formatted-device-list=\"'%i','%d','%v','%m','%t'%n\" 2>/dev/null";
  $logger->info($cmd);
 
  # Interface to frontend
@@ -69,42 +131,13 @@ sub get_devices {
     $logger->info("Reaped PID $pid");
     $running = FALSE;
 
-    $options{finished_callback}
-      ->( Gscan2pdf::Frontend::CLI->parse_device_list($output) )
-      if ( defined $options{finished_callback} );
-
+    $finished_callback->($output) if ( defined $finished_callback );
     return Glib::SOURCE_REMOVE;
    }
    return Glib::SOURCE_CONTINUE;
   }
  );
  return;
-}
-
-sub parse_device_list {
- my ( $class, $output ) = @_;
-
- my (@device_list);
-
- $logger->info($output) if ( defined($output) );
-
- # parse out the device and model names
- my @words =
-   &parse_line( ',', 0, substr( $output, 0, index( $output, "'\n" ) + 1 ) );
- while ( @words == 5 ) {
-  $output = substr( $output, index( $output, "'\n" ) + 2, length($output) );
-  push @device_list,
-    {
-   name   => $words[1],
-   vendor => $words[2],
-   model  => $words[3],
-   type   => $words[4]
-    };
-  @words =
-    &parse_line( ',', 0, substr( $output, 0, index( $output, "'\n" ) + 1 ) );
- }
-
- return \@device_list;
 }
 
 1;
