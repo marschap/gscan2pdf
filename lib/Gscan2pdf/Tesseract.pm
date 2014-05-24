marschap/gscan2pdf

commit ae76b3cf3f04fb6ed5d06609746d1993ff1396c1

Add support for tesseract 3.03

History
@@ -5,6 +5,8 @@
 * Speed up downsample option by using imagemagick sample instead of resize
 * Remove unnecessary decode_utf8() call causing
   'Cannot decode string with wide characters' error in Perl 5.18.
+* Add support for tesseract 3.03.
+  Closes bug #173 "v1.2.4 doesn't display any output"
 
 
 1.2.4 (26.03.2014):

lib/Gscan2pdf/Tesseract.pm
@@ -185,8 +185,16 @@ sub hocr {
  if ( not $setup ) { Gscan2pdf::Tesseract->setup($logger) }
 
  # Temporary filename for output
- my $suffix =
-   version->parse("v$version") >= version->parse("v3") ? '.html' : '.txt';
+ my $suffix;
+ if ( version->parse("v$version") >= version->parse("v3.02") ) {
+  $suffix = '.hocr';
+ }
+ elsif ( version->parse("v$version") >= version->parse("v3") ) {
+  $suffix = '.html';
+ }
+ else {
+  $suffix = '.txt';
+ }
  my $txt = File::Temp->new( SUFFIX => $suffix );
  ( $name, $path, undef ) = fileparse( $txt, $suffix );
 

commit 67d0cee9e6a3c2ebf71a933c7cb32becfa873229

Fix support for tesseract 3.02, broken by commit ae76b3cf

History
@@ -1,3 +1,8 @@
+1.2.6 (17.06.2014):
+
+* Fix support for tesseract 3.02, broken by commit ae76b3cf.
+
+
 1.2.5 (17.05.2014):
 
 * Fix today button on date dialog saving PDF

lib/Gscan2pdf/Tesseract.pm
@@ -193,7 +193,7 @@ sub hocr {
 
     # Temporary filename for output
     my $suffix;
-    if ( version->parse("v$version") >= version->parse("v3.02") ) {
+    if ( version->parse("v$version") >= version->parse("v3.03") ) {
         $suffix = '.hocr';
     }
     elsif ( version->parse("v$version") >= version->parse("v3") ) {