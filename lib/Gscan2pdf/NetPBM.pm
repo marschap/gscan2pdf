a2644b9a0f451dac1373a1937985cb4e2900f71d
Jeffrey Ratcliffe <Jeffrey.Ratcliffe@gmail.com>
2017-10-24 00:19:11 +0200

    Fix 16-bit PNM parsing

--- lib/Gscan2pdf/NetPBM.pm
+++ lib/Gscan2pdf/NetPBM.pm
@@ -3,12 +3,12 @@ package Gscan2pdf::NetPBM;
 use strict;
 use warnings;
 use Readonly;
-Readonly my $BINARY_BITMAP           => 4;
-Readonly my $BINARY_GRAYMAP          => 5;
-Readonly my $BITS_PER_BYTE           => 8;
-Readonly my $BITMAP_BYTES_PER_PIXEL  => 1 / $BITS_PER_BYTE;
-Readonly my $GRAYMAP_BYTES_PER_PIXEL => 1;
-Readonly my $PIXMAP_BYTES_PER_PIXEL  => 3;
+Readonly my $BINARY_BITMAP          => 4;
+Readonly my $BINARY_GRAYMAP         => 5;
+Readonly my $BITS_PER_BYTE          => 8;
+Readonly my $BITMAP_BYTES_PER_PIXEL => 1 / $BITS_PER_BYTE;
+Readonly my $GRAYMAP_CHANNELS       => 1;
+Readonly my $PIXMAP_CHANNELS        => 3;
 
 our $VERSION = '1.8.7';
 
@@ -16,51 +16,60 @@ our $VERSION = '1.8.7';
 
 sub file_size_from_header {
     my $filename = shift;
+    my ( $magic_value, $width, $height, $bytes_per_channel, $header ) =
+      read_header($filename);
+    if ( not defined $magic_value ) { return 0 }
+    if ( $magic_value == $BINARY_BITMAP ) {
+        my $mod = $width % $BITS_PER_BYTE;
+        if ( $mod > 0 ) { $width += $BITS_PER_BYTE - $mod }
+    }
+    my $datasize = $width * $height * $bytes_per_channel * (
+        $magic_value == $BINARY_BITMAP ? $BITMAP_BYTES_PER_PIXEL
+        : (
+              $magic_value == $BINARY_GRAYMAP ? $GRAYMAP_CHANNELS
+            : $PIXMAP_CHANNELS
+        )
+    );
+    return $header + $datasize;
+}
 
-    open my $fh, '<', $filename or return 0;
+sub read_header {
+    my $filename = shift;
+
+    open my $fh, '<', $filename or return;
     my $header = <$fh>;
-    my $magic_value;
+    my ( $magic_value, $width, $height, $line );
+    my $bytes_per_channel = 1;
     if ( defined $header and $header =~ /^P(\d)\n/xsm ) {
         $magic_value = $1;
     }
     else {
-        close $fh or return 0;
-        return 0;
+        close $fh or return;
+        return;
     }
     if ( $magic_value < $BINARY_BITMAP ) {
-        close $fh or return 0;
-        return 0;
+        close $fh or return;
+        return;
     }
-    my $line = <$fh>;
-    $header .= $line;
-    while ( $line =~ /^(\#|\s*\n)/xsm ) {
-        $line = <$fh>;
+    while ( $line = <$fh> ) {
         $header .= $line;
-    }
-    if ( $line =~ /(\d*)[ ](\d*)\n/xsm ) {
-        my ( $width, $height ) = ( $1, $2 );
-        if ( $magic_value == $BINARY_BITMAP ) {
-            my $mod = $width % $BITS_PER_BYTE;
-            if ( $mod > 0 ) { $width += $BITS_PER_BYTE - $mod }
+        if ( $line =~ /^(\#|\s*\n)/xsm ) { next }
+        if ( $line =~ /(\d*)[ ](\d*)\n/xsm ) {
+            ( $width, $height ) = ( $1, $2 );
+            if ( $magic_value == $BINARY_BITMAP ) { last }
         }
-        my $datasize = $width * $height * (
-            $magic_value == $BINARY_BITMAP ? $BITMAP_BYTES_PER_PIXEL
-            : (
-                  $magic_value == $BINARY_GRAYMAP ? $GRAYMAP_BYTES_PER_PIXEL
-                : $PIXMAP_BYTES_PER_PIXEL
-            )
-        );
-        if ( $magic_value > $BINARY_BITMAP ) {
-            $line = <$fh>;
-            $header .= $line;
+        elsif ( $magic_value > $BINARY_BITMAP and $line =~ /(\d+)\n/xsm ) {
+            my $maxval = $1;
+            $bytes_per_channel = log( $maxval + 1 ) / log(2) / $BITS_PER_BYTE;
+            last;
         }
-        close $fh or return 0;
-        return length($header) + $datasize;
+        else {
+            close $fh or return;
+            return;
+        }
     }
-    else {
-        close $fh or return 0;
-        return 0;
-    }
+    close $fh or return;
+    return $magic_value, $width, $height, $bytes_per_channel, length $header;
 }
 
 1;
--- t/01_NetPBM.t
+++ t/01_NetPBM.t
@@ -1,6 +1,6 @@
 use warnings;
 use strict;
-use Test::More tests => 8;
+use Test::More tests => 14;
 
 BEGIN {
     use_ok('Gscan2pdf::NetPBM');
@@ -8,15 +8,16 @@ BEGIN {
 
 #########################
 
-for (qw(pbm pgm ppm)) {
-    my $file = "test.$_";
-    system("convert -resize 8x5 rose: $file");
-    is( Gscan2pdf::NetPBM::file_size_from_header($file),
-        -s $file, "get_size_from_PNM $_ 8 wide" );
-    system("convert -resize 9x6 rose: $file");
-    is( Gscan2pdf::NetPBM::file_size_from_header($file),
-        -s $file, "get_size_from_PNM $_ 9 wide" );
-    unlink $file;
+for my $type (qw(pbm pgm ppm)) {
+    for my $depth ( ( 8, 16 ) ) {
+        for my $size ( ( "8x5", "9x6" ) ) {
+            my $file = "test.$type";
+            system("convert -depth $depth -resize $size rose: $file");
+            is( Gscan2pdf::NetPBM::file_size_from_header($file),
+                -s $file, "get_size_from_PNM $type $size depth $depth" );
+            unlink $file;
+        }
+    }
 }
 
 #########################
