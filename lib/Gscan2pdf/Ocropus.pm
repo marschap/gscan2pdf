41e9a451514a67348fb575f84309e2f5855668cc
Jeffrey Ratcliffe <jffry@posteo.net>
2017-10-29 19:22:34 +0100

    Bumped version to 1.8.8

--- lib/Gscan2pdf/Ocropus.pm
+++ lib/Gscan2pdf/Ocropus.pm
@@ -10,7 +10,7 @@ use HTML::Entities;
 use Encode;
 use English qw( -no_match_vars );    # for $PROCESS_ID
 
-our $VERSION = '1.8.7';
+our $VERSION = '1.8.8';
 
 my ( $exe, $installed, $setup, $logger );
 
