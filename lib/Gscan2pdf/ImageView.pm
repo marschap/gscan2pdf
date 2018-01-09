eb60a906a2bd5389cb5e8ce8d81e5460baf8f013
Jeffrey Ratcliffe <Jeffrey.Ratcliffe@gmail.com>
2018-01-09 10:57:49 +0100

    + zoom* functions to ImageView

--- lib/Gscan2pdf/ImageView.pm
+++ lib/Gscan2pdf/ImageView.pm
@@ -1,21 +1,34 @@
 package Gscan2pdf::ImageView;
 
-use strict;
 use warnings;
+use strict;
+no if $] >= 5.018, warnings => 'experimental::smartmatch';
 use feature 'switch';
 use Glib qw(TRUE FALSE);    # To get TRUE and FALSE
 use Gtk3;
 use List::Util qw(min);
 
-use Glib::Object::Subclass Gtk3::ScrolledWindow::,
-  signals    => {},
+use Glib::Object::Subclass Gtk3::ScrolledWindow::, signals => {
+    'zoom-changed' => {
+        param_types => ['Glib::Float'],    # new zoom
+    },
+  },
   properties => [
     Glib::ParamSpec->object(
-        'pixbuf',                         # name
-        'pixbuf',                         # nickname
-        'Pixbuf of image to be shown',    # blurb
-        'Gtk3::Gdk::Pixbuf',              # package
-        [qw/readable writable/]           # flags
+        'pixbuf',                          # name
+        'pixbuf',                          # nickname
+        'Pixbuf of image to be shown',     # blurb
+        'Gtk3::Gdk::Pixbuf',               # package
+        [qw/readable writable/]            # flags
+    ),
+    Glib::ParamSpec->float(
+        'zoom',                            # name
+        'zoom',                            # nick
+        'zoom level',                      # blurb
+        0.001,                             # minimum
+        1000.0,                            # maximum
+        1.0,                               # default_value
+        [qw/readable writable/]            # flags
     ),
   ];
 
@@ -25,33 +38,98 @@ sub INIT_INSTANCE {
     $self->set_vexpand(1);
     $self->{image} = Gtk3::Image->new();
     $self->add_with_viewport( $self->{image} );
+    $self->signal_connect( 'zoom-changed' => sub { $self->_update_image } );
     return $self;
 }
 
-sub set_pixbuf {
-    my ( $self, $pixbuf ) = @_;
-    $self->set( 'pixbuf', $pixbuf );
-    $self->update_image;
+sub SET_PROPERTY {
+    my ( $self, $pspec, $newval ) = @_;
+    my $name   = $pspec->get_name;
+    my $oldval = $self->get($name);
+    if (   ( defined $newval and defined $oldval and $newval ne $oldval )
+        or ( defined $newval xor defined $oldval ) )
+    {
+        given ($name) {
+            when ('pixbuf') {
+                $self->{$name} = $newval;
+            }
+            when ('zoom') {
+                $self->{$name} = $newval;
+                $self->signal_emit( 'zoom-changed', $newval );
+            }
+            default {
+                $self->SUPER::SET_PROPERTY( $pspec, $newval );
+            }
+        }
+    }
     return;
 }
 
-sub update_image {
+sub set_pixbuf {
+    my ( $self, $pixbuf, $zoom_to_fit ) = @_;
+    $self->set( 'pixbuf', $pixbuf );
+    if ($zoom_to_fit) {
+        $self->zoom_to_fit;
+    }
+    else {
+        $self->_update_image;
+    }
+    return;
+}
+
+sub get_pixbuf {
+    my ($self) = @_;
+    return $self->get('pixbuf');
+}
+
+sub _update_image {
     my ($self) = @_;
     my $pixbuf = $self->get('pixbuf');
     if ( not defined $pixbuf ) { return }
-    my $max_w  = $self->get_allocation()->{width};
-    my $max_h  = $self->get_allocation()->{height};
     my $pixb_w = $pixbuf->get_width();
     my $pixb_h = $pixbuf->get_height();
-    if ( $pixb_w > $max_w or $pixb_h > $max_h ) {
-        my $sc_factor_w = $max_w / $pixb_w;
-        my $sc_factor_h = $max_h / $pixb_h;
-        my $sc_factor   = min( $sc_factor_w, $sc_factor_h );
-        my $sc_w        = int( $pixb_w * $sc_factor );
-        my $sc_h        = int( $pixb_h * $sc_factor );
-        my $pixbuf = $pixbuf->scale_simple( $sc_w, $sc_h, 'GDK_INTERP_HYPER' );
-    }
+    my $zoom   = $self->get('zoom');
+    my $sc_w   = int( $pixb_w * $zoom );
+    my $sc_h   = int( $pixb_h * $zoom );
+    $pixbuf = $pixbuf->scale_simple( $sc_w, $sc_h, 'GDK_INTERP_HYPER' );
     $self->{image}->set_from_pixbuf($pixbuf);
+    return;
+}
+
+sub set_zoom {
+    my ( $self, $zoom ) = @_;
+    $self->set( 'zoom', $zoom );
+    return;
+}
+
+sub get_zoom {
+    my ($self) = @_;
+    return $self->get('zoom');
+}
+
+sub zoom_to_fit {
+    my ($self) = @_;
+    my $pixbuf = $self->get('pixbuf');
+    if ( not defined $pixbuf ) { return }
+    my $max_w       = $self->get_allocation->{width};
+    my $max_h       = $self->get_allocation->{height};
+    my $pixb_w      = $pixbuf->get_width();
+    my $pixb_h      = $pixbuf->get_height();
+    my $sc_factor_w = $max_w / $pixb_w;
+    my $sc_factor_h = $max_h / $pixb_h;
+    $self->set( 'zoom', min( $sc_factor_w, $sc_factor_h ) );
+    return;
+}
+
+sub zoom_in {
+    my ($self) = @_;
+    $self->set( 'zoom', $self->get('zoom') * 2 );
+    return;
+}
+
+sub zoom_out {
+    my ($self) = @_;
+    $self->set( 'zoom', $self->get('zoom') / 2 );
     return;
 }
 
---
+++ t/65_imageview.t
@@ -0,0 +1,123 @@
+use Test::More tests => 29;
+
+BEGIN {
+    use Glib qw/TRUE FALSE/;
+    use Gtk3 -init;
+    use_ok('Gscan2pdf::ImageView');
+}
+
+#########################
+
+my $view = Gscan2pdf::ImageView->new;
+ok( defined $view, 'new() works' );
+isa_ok( $view, 'Gscan2pdf::ImageView' );
+
+SKIP: {
+    skip 'not yet', 2;
+    ok( defined $view->get_tool, 'get_tool() works' );
+
+    # The default tool is Gscan2pdf::ImageView::Tool::Dragger.
+    isa_ok( $view->get_tool, 'Gscan2pdf::ImageView::Tool::Dragger' );
+}
+
+system('convert rose: test.jpg');
+$view->set_pixbuf( Gtk3::Gdk::Pixbuf->new_from_file('test.jpg'), TRUE );
+
+SKIP: {
+    skip 'not yet', 3;
+    isa_ok( $view->get_viewport, 'Gtk3::Gdk::Rectangle' );
+
+    isa_ok( $view->get_draw_rect, 'Gtk3::Gdk::Rectangle' );
+
+    ok( $view->get_check_colors, 'get_check_colors() works' );
+}
+
+ok( defined $view->get_pixbuf, 'get_pixbuf() works' );
+
+ok( defined $view->get_zoom, 'get_zoom() works' );
+
+my $signal = $view->signal_connect(
+    'zoom-changed' => sub { pass 'emitted zoom-changed signal' } );
+$view->set_zoom(1);
+
+SKIP: {
+    skip 'not yet', 5;
+    ok(
+        Gscan2pdf::ImageView::Zoom->get_min_zoom <
+          Gscan2pdf::ImageView::Zoom->get_max_zoom,
+'Ensure that the gtkimageview.zooms_* functions are present and work as expected.'
+    );
+
+    ok( defined $view->get_black_bg, 'get_black_bg() works' );
+
+    ok( defined $view->get_show_frame, 'get_show_frame() works' );
+
+    ok( defined $view->get_interpolation, 'get_interpolation() works' );
+
+    ok( defined $view->get_show_cursor, 'get_show_cursor() works' );
+}
+
+eval { $view->set_pixbuf( 'Hi mom!', TRUE ) };
+like( $@, qr/type/,
+'A TypeError is raised when set_pixbuf() is called with something that is not a pixbuf.'
+);
+
+$view->set_pixbuf( undef, TRUE );
+ok( !$view->get_pixbuf, 'correctly cleared pixbuf' );
+
+SKIP: {
+    skip 'not yet', 11;
+    ok( !$view->get_viewport, 'correctly cleared viewport' );
+
+    ok( !$view->get_draw_rect, 'correctly cleared draw rectangle' );
+
+    $view->size_allocate( Gtk3::Gdk::Rectangle->new( 0, 0, 100, 100 ) );
+    $view->set_pixbuf(
+        Gtk3::Gdk::Pixbuf->new( GDK_COLORSPACE_RGB, FALSE, 8, 50, 50 ) );
+    my $rect = $view->get_viewport;
+    ok(
+        (
+                  $rect->x == 0 and $rect->y == 0
+              and $rect->width == 50
+              and $rect->height == 50
+        ),
+        'Ensure that getting the viewport of the view works as expected.'
+    );
+
+    can_ok( $view, qw(get_check_colors) );
+
+    $rect = $view->get_draw_rect;
+    ok(
+        (
+                  $rect->x == 25 and $rect->y == 25
+              and $rect->width == 50
+              and $rect->height == 50
+        ),
+        'Ensure that getting the draw rectangle works as expected.'
+    );
+
+    $view->set_pixbuf(
+        Gtk3::Gdk::Pixbuf->new( GDK_COLORSPACE_RGB, FALSE, 8, 200, 200 ) );
+    $view->set_zoom(1);
+    $view->set_offset( 0, 0 );
+    $rect = $view->get_viewport;
+    ok( ( $rect->x == 0 and $rect->y == 0 ),
+        'Ensure that setting the offset works as expected.' );
+
+    $view->set_offset( 100, 100, TRUE );
+    $rect = $view->get_viewport;
+    ok( ( $rect->x == 100 and $rect->y == 100 ),
+        'Ensure that setting the offset works as expected.' );
+
+    $view->set_transp( 'color', 0xff0000 );
+    my ( $col1, $col2 ) = $view->get_check_colors;
+    ok(
+        ( $col1 == 0xff0000 and $col2 == 0xff0000 ),
+        'Ensure that setting the views transparency settings works as expected.'
+    );
+    $view->set_transp('grid');
+
+    ok( defined Glib::Type->list_values('Gscan2pdf::ImageView::Transp'),
+        'Check GtkImageTransp enum.' );
+}
+unlink 'test.jpg';
